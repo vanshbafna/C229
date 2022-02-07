92600f62a5614d66a3e4053474b0e37b536a9904
Creative Devloper(Vansh Bafna) <59309266+vanshbafna@users.noreply.github.com>
2022-02-07 22:14:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import hashlib
+from itertools import permutations
+
+def find_hash(original_hash):
+    word_file = open("words.txt","r")
+    word_file = list(word_file)
+
+    anagram = "who outlay thieves"
+    words = anagram.count(' ')
+    words += 1
+
+    char_list = list(set(anagram))
+
+    if ' ' in char_list:
+        char_list.remove(' ')
+
+    final_words = []
+
+    #Student Activity
+
+    print(len(final_words))
+
+    for elem in permutations(final_words, words):
+        hash_elem = " ".join(elem)
+        
+        #Student Activity
+        
+        m = hashlib.md5()
+        m.update(hash_elem.encode('utf-8'))
+        word_hash = m.hexdigest()
+
+        if word_hash == original_hash:
+            return hash_elem
+
+hash = '13b382e1a2f8e22535b4730d78bc8591'
+answer = find_hash(hash)
+print(f"Collision!  The word corresponding to the given hash is '{answer}'")
